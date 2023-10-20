47753b0dda529a59acb9f38d4776824a26210856
Andres <95327624+scarfon@users.noreply.github.com>
2023-10-19 22:24:03 -0300

    first comit fashion wear

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Image style={styles.img} source={require("./assets/logo.png")} />
+			<Text style={styles.titulo}>Faça seu login</Text>
+			<Text style={{ fontSize: 18 }}>Para começar a vender</Text>
+			<TextInput style={styles.input} placeholder="e-mail" />
+			<TextInput style={styles.input} placeholder="Senha" secureTextEntry />
+			<Text style={styles.botao}>Entrar</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#FCFBFF",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	titulo: {
+		color: "#121a2c",
+		fontSize: 28,
+		fontWeight: "bold",
+	},
+	input: {
+		paddingVertical: 12,
+		paddingHorizontal: 16,
+		borderRadius: 4,
+		borderColor: "#747474",
+		borderWidth: 1,
+		marginVertical: 20,
+		width: "50%",
+	},
+	botao: {
+		backgroundColor: "black",
+		color: "#FFFFFF",
+		paddingVertical: 8,
+		paddingHorizontal: 60,
+		borderRadius: 4,
+	},
+	img: {
+		width: "100%",
+		height: 300,
+		position: "absolute",
+		top: 0,
+	},
 });
